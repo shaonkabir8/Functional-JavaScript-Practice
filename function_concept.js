24819ba37fbd63c0136ae6afdea0c8b7744cbb2d
Shaon kabir <shaonkabir98@gmail.com>
2019-05-06 08:19:10 -0800

    Basic Function Concept and It's Speciallity in JavaScript

---
+++ function_concept.js
@@ -0,0 +1,30 @@
+// Basic Function ==>
+// Practice_Source ==> FunFunFunction :)
+
+/* 
+
+    In JavaScript , Function is a first class function.Also an object too.
+
+
+    # Function can be stored in a variable
+    # Function can be stored in an Array
+    # Function can be stored in an object
+    # Function can be passed as an Argument/Parameter
+    # Function can return another Function .
+
+// Basic Function Frame ==>
+
+var FunctionName = function(args) {
+    // Statements;
+};
+FunctionName();
+
+or ,
+
+function FunctionName(args) {
+    // Statements;
+};
+FunctionName();
+
+
+*/
