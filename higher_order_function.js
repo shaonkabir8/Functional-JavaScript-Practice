0f78eb8ff0357a687e2f60f859fac4e568bcf6fb
Shaon kabir <shaonkabir98@gmail.com>
2019-05-06 08:43:05 -0800

    Filter Function

--- higher_order_function.js
+++ higher_order_function.js
@@ -8,3 +8,32 @@ Higher Order Function ==>
 
 Curtecy : FunFunFunction.
 */
+
+
+/* 
+    Filter Function ==>
+*/
+// Traditional way using loop
+var animals = [
+    {name:'Tommy' , specis:'Dog'},
+    {name:'Pussy' , specis:'Cat'},
+    {name:'Flying Bird' , specis:'Pigeon'},
+    {name:'Horn Fish' , specis:'Fish'},
+    {name:'Shark' , specis:'Fish'},
+    {name:'Dove' , specis:'Bird'},
+    {name:'Cocatails' , specis:'Bird'}
+];
+var newAnimal = [];
+for (var i = 0; i<animals.length; i++){
+    if(animals[i].name === "Shark") {
+        newAnimal.push(animals[i]);
+    }
+};
+console.log(newAnimal);
+
+// using ==> filter function <==
+
+var anotherContainer = animals.filter(function(animalName){
+    return animalName.name === "Dove";
+});
+console.log(anotherContainer);
